bb6abb4c79daf085730c7813c976bcd214ac4e6e
Jesse Brandenburg <jesselbrandenburg@gmail.com>
2013-11-22 14:50:10 -0600

    Implemented all pre-existing tests.

--- spec/tennis_spec.rb
+++ spec/tennis_spec.rb
@@ -1,23 +1,26 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'rspec'
+require 'pry-plus'
 require_relative '../tennis'
 
 describe Tennis::Game do
   let(:game) { Tennis::Game.new }
-
   describe '.initialize' do
     it 'creates two players' do
       expect(game.player1).to be_a(Tennis::Player)
       expect(game.player2).to be_a(Tennis::Player)
     end
 
-    it 'sets the opponent for each player'
+    it 'sets the opponent for each player' do
+      expect(game.player1.opponent).to eq(game.player2)
+      expect(game.player2.opponent).to eq(game.player1)
+    end
   end
 
   describe '#wins_ball' do
     it 'increments the points of the winning player' do
-      game.wins_ball(1)
+      game.wins_ball(game.player1)
 
       expect(game.player1.points).to eq(1)
     end
@@ -62,11 +65,19 @@ describe Tennis::Player do
     end
     
     context 'when points is 2' do
-      it 'returns thirty'  
+      it 'returns thirty'  do
+        player.points = 2
+
+        expect(player.score).to eq('thirty')
+      end
     end
     
     context 'when points is 3' do
-      it 'returns forty' 
+      it 'returns forty' do
+        player.points = 3
+
+        expect(player.score).to eq('forty')
+      end
     end
   end
 end
